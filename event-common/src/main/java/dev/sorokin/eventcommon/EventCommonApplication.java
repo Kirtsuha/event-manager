116b95deb2d84ba1d64fd0b5fe6ea913fbe2c5c9
Kirtsuha <batukhtin.kir@gmail.com>
2026-04-21 19:38:03 +0300

    updated structure for event notificator

---
+++ event-common/src/main/java/dev/sorokin/eventcommon/EventCommonApplication.java
@@ -0,0 +1,13 @@
+package dev.sorokin.eventcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCommonApplication.class, args);
+	}
+
+}
